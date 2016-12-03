b02c96d6a0a7a63ca53d888332c04d9f3cf08505
mwear3robloxandroid <mitchellwears@gmail.com>
2016-12-03 05:07:29 +0000

    Update menugui.C

--- menu/menugui.C
+++ menu/menugui.C
@@ -12,3 +12,24 @@ public class ExampleClass : MonoBehaviour {
         GUI.Button(new Rect(0, 0, 100, 20), new GUIContent(https://avatars0.githubusercontent.com/u/24334937?v=3&s=70));
     }
 }
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class Example : MonoBehaviour 
+{
+	public Image pauseMenu;
+	
+	public void Start()
+	{
+		//Enableusing UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+    void Start() {
+        HostData[] hostData = MasterServer.PollHostList();
+        foreach (HostData element in hostData) {
+            Debug.Log(element.gameType);
+        }
+    }
+}                                              
